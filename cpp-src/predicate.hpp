7f83c81d6f181cc42f5de02ab201b156885b76eb
Heman Gandhi <hemangandhi@gmail.com>
2019-11-29 00:46:29 -0500

    numeric predicates

--- cpp-src/predicate.hpp
+++ cpp-src/predicate.hpp
@@ -13,7 +13,7 @@ virtual class Predicate{
 };
 
 template <typename Structure>
-class WrappedFunction: public Predicate {
+class WrappedFunction: public Predicate<Structure> {
     public:
         explicit WrappedFunction(std::function<const Structure&, bool> pred) : predicate(pred) {}
 
@@ -34,32 +34,80 @@ class WrappedFunction: public Predicate {
  */
 
 template <typename Structure>
-operator& WrappedFunction<Structure>(const Predicate& p1, const Predicate& p2){
+operator& WrappedFunction<Structure>(const Predicate<Structure>& p1, const Predicate<Structure>& p2){
     return WrappedFunction([=](const Structure& s){
         return p1.Validate(s) && p2.Validate(s);
     });
 }
 
 template <typename Structure>
-operator| WrappedFunction<Structure>(const Predicate& p1, const Predicate& p2){
+operator| WrappedFunction<Structure>(const Predicate<Structure>& p1, const Predicate<Structure>& p2){
     return WrappedFunction([=](const Structure& s){
         return p1.Validate(s) || p2.Validate(s);
     });
 }
 
 template <typename Structure>
-operator^ WrappedFunction<Structure>(const Predicate& p1, const Predicate& p2){
+operator^ WrappedFunction<Structure>(const Predicate<Structure>& p1, const Predicate<Structure>& p2){
     return WrappedFunction([=](const Structure& s){
         return p1.Validate(s) ^ p2.Validate(s);
     });
 }
 
 template <typename Structure>
-operator~ WrappedFunction<Structure>(const Predicate& p1){
+operator~ WrappedFunction<Structure>(const Predicate<Structure>& p1){
     return WrappedFunction([=](const Structure& s){
         return !p1.Validate(s);
     });
 }
+
+// A predicate on numbers, looking at comparisons, divisibility,
+// and primality. The static creators are easier to use than the
+// constructor, but the constructor is public for masochists.
+class NumericPred: public Predicate<int> {
+    public:
+        bool Validate(const int& in){
+            if(div_int != 0 && in % div_int != 0) return false;
+            if(upper_bound.has_value() && in > upper_bound) return false;
+            if(lower_bound.has_value() && in < lower_bound) return false;
+            return !prime || CheckPrime(in);
+        }
+
+    // TODO: custom (specialized?) overloads of the above &, |, ^, ~ to
+    // not bloat memory with wacky lambda thingies and just make the good validator.
+
+    // Factories! Make your numeric predicates in a lyrical English-sounding interface!
+    static NumericPred IsNumber(int n){
+        // Abusing the inclusivity of the bounding stuff
+        return NumericPred(0, {n}, {n}, false);
+    }
+    static NumericPred IsAtLeast(int n){
+        return NumericPred(0, {std::nullopt_t}, {n}, false);
+    }
+    static NumericPred IsAtMost(int n){
+        return NumericPred(0, {n}, {std::nullopt_t}, false);
+    }
+    static NumericPred Divides(int n){
+        return NumericPred(n, {std::nullopt_t}, {std::nullopt_t}, false);
+    }
+    static NumericPred IsPrime(){
+        return NumericPred(0, {std::nullopt_t}, {std::nullopt_t}, true);
+    }
+
+    private:
+        // TODO: better ways to express stuff like "it's OK to Miller-Rabin"
+        // or something so that this isn't the trashiest. (Ooh, template param?)
+        bool CheckPrime(const int& in){
+            for(int i = 2; i < in; i++){ // Recall: loop won't run if in == 2
+                if(in % i == 0) return false;
+            }
+            return true;
+        }
+
+        int div_int;
+        std::optional<int> upper_bound, lower_bound;
+        bool prime
+};
 
 } // namespace predicate
 
