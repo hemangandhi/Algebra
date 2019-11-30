d57c6869d0a371b681313e9694cfb5f05b7704d1
Heman Gandhi <hemangandhi@gmail.com>
2019-11-29 23:30:26 -0500

    group-related predicate headers

--- cpp-src/group.hpp
+++ cpp-src/group.hpp
@@ -1,6 +1,7 @@
 #ifndef ALGEBRA_GROUP_H_
 #define ALGEBRA_GROUP_H_
 
+#include <memory>
 #include <optional>
 #include <string>
 #include <vector>
@@ -9,25 +10,67 @@
 
 namespace algebra {
 
+template <typename Element>
+virtual class Set{
+    virtual bool Contains(const Element& e) const = 0;
+    // returns nullopt_t to say that the set is "infinite"
+    virtual std::optional<std::vector<Element>> Enumerate() const = 0;
+}
+
 class Subgroup;
+class GroupPredicate;
 
 template <typename Element>
-virtual class Group {
-    virtual bool Contains(const Element& e) const = 0;
-    virtual std::optional<std::vector<Element>> UnderlyingSet() const = 0;
+virtual class Group : public Set<Element>{
     virtual Element Identity() const = 0;
     virtual Element Multiply(const Element& e1, const Element& e2) const = 0;
     virtual Element Invert(const Element& e) = 0;
 
-    virtual void GenerateSubgroups(predicate::GroupPredicate cond, std::vector<Subgroup *>* output);
+    // Tries to generate all the subgroups that satisfy a predicate. Returns true
+    // iff all subgroups were generated. The expectation is that the generated subgroups
+    // are moved out.
+    virtual bool GenerateSubgroups(GroupPredicate cond, std::vector<std::unique_ptr<Subgroup>>* output);
 };
 
 template <typename Element>
-virtual class Subgroup::Group<Element> {
+virtual class Subgroup: public Group<Element> {
     virtual const Group * GetParent() const = 0;
     virtual bool Normal() const = 0;
     virtual const Group * Quotient() const = 0;
 }
+
+template <typename Element>
+class SetPredicate: public predicate::Predicate<Set<Element>>{
+    public:
+        bool Validate(const Set<Element>& s) override {
+            return s.Contains(e);
+        }
+
+        static SetPredicate<Element> Contains(const Element& e){
+            return SetPredicate(e);
+        }
+    private:
+        Element expected;
+};
+
+template <typename Element>
+class ElementPredicate: public predicate::Predicate<Element>{
+        bool Validate(const Set<Element>& s) override;
+
+        static ElementPredicate<Element> OrderWhich(predicate::NumericPredicate is);
+        static ElementPredicate<Element> Generates(predicate::NumericPredicate is);
+    private:
+        std::optional<Group> generates;
+        std::optional<predicate::NumericPredicate> order_condition;
+};
+
+template <typename Element>
+class GroupPredicate: public SetPredicate {
+        bool Validate(const Set<Element>& s) override;
+    private:
+        ElementPredicate<Element> element;
+        std::unique_pointer<GroupPredicate> has_subgroup;
+};
 
 } // namespace algebra
 
